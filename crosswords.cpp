288d254053520d8df225242d7e5d65a61fa23105
Ammar Ali <m.ammarali98@gmail.com>
2020-08-12 20:32:45 +0500

    Overloaded addRow and addColumn of 2 & 3 args

--- crosswords.cpp
+++ crosswords.cpp
@@ -67,6 +67,7 @@ namespace crosswords
                 }
             }
         }
+
         bool addRow(int R, const wordList &inList, int A)
         {
             char B = board[R][A];
@@ -83,6 +84,61 @@ namespace crosswords
             return 1;
         }
 
+        bool addRow(int R, const wordList &inList, int A, int A1)
+        {
+            char B = board[R][A];
+            char B1 = board[R][A1];
+            auto fetched = inList.fetch(A, B);
+            std::string S = "";
+            for (auto word : fetched)
+            {
+                if (word[A1] == B1)
+                {
+                    LOG("addRow(R,A,A1) " + word);
+                    S = word;
+                    break;
+                }
+            }
+            if (S == "")
+            {
+                return 0;
+            }
+            setRow(R, S);
+            return 1;
+        }
+
+        bool addRow(int R, const wordList &inList, int A, int A1, int A2)
+        {
+            char B = board[R][A];
+            char B1 = board[R][A1];
+            char B2 = board[R][A2];
+            auto fetched = inList.fetch(A, B);
+            std::vector<std::string> S_fetched;
+            for (auto word : fetched)
+            {
+                if (word[A1] == B1)
+                {
+                    S_fetched.push_back(word);
+                }
+            }
+            std::string S = "";
+            for (auto word : S_fetched)
+            {
+                if (word[A2] == B2)
+                {
+                    LOG("addRow(R,A,A1,A2) " + word);
+                    S = word;
+                    break;
+                }
+            }
+            if (S == "")
+            {
+                return 0;
+            }
+            setRow(R, S);
+            return 1;
+        }
+
         bool addColumn(int C, const wordList &inList, int A)
         {
             char B = board[A][C];
@@ -95,7 +151,62 @@ namespace crosswords
             {
                 return 0;
             }
-            setRow(C, fetched[puzzleSeed % fetched.size()]);
+            setColumn(C, fetched[puzzleSeed % fetched.size()]);
+            return 1;
+        }
+
+        bool addColumn(int C, const wordList &inList, int A, int A1)
+        {
+            char B = board[A][C];
+            char B1 = board[A1][C];
+            auto fetched = inList.fetch(A, B);
+            std::string S = "";
+            for (auto word : fetched)
+            {
+                if (word[A1] == B1)
+                {
+                    LOG("addColumn(C,A,A1) " + word);
+                    S = word;
+                    break;
+                }
+            }
+            if (S == "")
+            {
+                return 0;
+            }
+            setColumn(C, S);
+            return 1;
+        }
+
+        bool addColumn(int C, const wordList &inList, int A, int A1, int A2)
+        {
+            char B = board[A][C];
+            char B1 = board[A1][C];
+            char B2 = board[A2][C];
+            auto fetched = inList.fetch(A, B);
+            std::vector<std::string> S_fetched;
+            for (auto word : fetched)
+            {
+                if (word[A1] == B1)
+                {
+                    S_fetched.push_back(word);
+                }
+            }
+            std::string S = "";
+            for (auto word : S_fetched)
+            {
+                if (word[A2] == B2)
+                {
+                    LOG("addColumn(C,A,A1,A2) " + word);
+                    S = word;
+                    break;
+                }
+            }
+            if (S == "")
+            {
+                return 0;
+            }
+            setColumn(C, S);
             return 1;
         }
 
