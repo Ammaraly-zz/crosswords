ac39cea0bc6f6dad8c197b1ca0fd992a6d2b82ae
Ammar Ali <m.ammarali98@gmail.com>
2020-08-12 15:59:54 +0500

    Added Debugging and logging prepocessor directives

--- crosswords.cpp
+++ crosswords.cpp
@@ -1,3 +1,12 @@
+#define CW_DEBUG 1
+#if CW_DEBUG == 1
+#define LOG(x) std::cout << "[LOG] " << x << '\n'
+#define ERROR(x) std::cout << "[ERROR] " << x << '\n'
+#else
+#define LOG(x)
+#define ERROR(x)
+#endif
+
 #include<iostream>
 #include <unistd.h>
 #include<fstream>
@@ -22,7 +31,7 @@ int main(int argc, char* argv[]) {
      }
 
      for(std::string word : wordList) {
-         std::cout << "[" << word << "]\n";
+         LOG(word);
      }
      return 0;
 }
