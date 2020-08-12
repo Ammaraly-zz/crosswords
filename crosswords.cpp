5d6405bfb22e14cc90e43e8f42470b72edcaf949
Ammar Ali <m.ammarali98@gmail.com>
2020-08-12 17:19:58 +0500

    4 arrays sorted according to Nth letter

--- crosswords.cpp
+++ crosswords.cpp
@@ -7,32 +7,40 @@
 #define ERROR(x)
 #endif
 
-#include<iostream>
+#include <iostream>
 #include <unistd.h>
-#include<fstream>
-#include<vector>
-#include<array>
+#include <fstream>
+#include <vector>
+#include <array>
 
-int main(int argc, char* argv[]) {
-     bool filecheck = access(argv[1], F_OK);
-     std::ifstream wordListFile;
-     std::array<std::vector<std::string>,26> splitWordList;
-     std::string line;
+int main(int argc, char *argv[])
+{
+    bool filecheck = access(argv[1], F_OK);
+    std::ifstream wordListFile;
+    std::array<std::array<std::vector<std::string>, 26>, 4> splitWordList;
+    std::string line;
 
-     if(!filecheck) {
-         std::cout << "[LOG] FILE EXISTS\n";
-         wordListFile.open(argv[1]);
-     }
-     else {
-         std::cout << "[ERROR] NO SUCH FILE!\n";
-         return 69;
-     }
-     while(std::getline(wordListFile,line)) {
-         splitWordList[line[0]-65].push_back(line);
-     }
+    if (!filecheck)
+    {
+        std::cout << "[LOG] FILE EXISTS\n";
+        wordListFile.open(argv[1]);
+    }
+    else
+    {
+        std::cout << "[ERROR] NO SUCH FILE!\n";
+        return 69;
+    }
+    while (std::getline(wordListFile, line))
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            splitWordList[i][line[i] - 65].push_back(line);
+        }
+    }
 
-     for(std::string word : splitWordList['C'-65]) {
-         LOG(word);
-     }
-     return 0;
+    for (std::string word : splitWordList[3]['C' - 65])
+    {
+        LOG(word);
+    }
+    return 0;
 }
